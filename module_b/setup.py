bb2bbd63dc3f82ab6f4ce17cabb88446b2d7dc41
Anna Liu <sonoanna@gmail.com>
2022-12-02 16:27:29 +0800

    a

--- module_b/setup.py
+++ module_b/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='TestLibrary',
+    name='module_b',
     version='0.0.0',
     author='Anna Liu',
     author_email='sonoanna@gmail.com',
     description='Testing installation of Package',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/anfyanna/TestLibrary',
+    url='https://github.com/anfyanna/TestLibrary/module_b',
     project_urls = {
         "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
     },
