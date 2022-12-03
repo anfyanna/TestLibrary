9672abbf0c5bb20a7bd3a78c3770d59e5361a8b5
Anna Liu <sonoanna@gmail.com>
2022-12-04 00:56:34 +0800

    a

--- PKTools/pk_tools/utilities/string.py
+++ PKTools/pk_tools/utilities/string.py
@@ -1,4 +1,5 @@
 import random
+import hashlib
 
 
 def generate_string(length: str = 6, chars: str = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'):
@@ -21,6 +22,54 @@ def strB2Q(ustring):
             inside_code += 65248
         rstring += chr(inside_code)
     return rstring
+
+
+def amt_with_thousandths(amt: str) -> str:
+    '''
+    數字加上千分位逗號
+
+    Args:
+        amt (str): 數字字串
+
+    Returns:
+        str: 數字字串
+    '''
+    try:
+        value = int(amt)
+        result = format(value, ',')
+        return result
+    except Exception:
+        return amt
+
+
+def md5_hash(string):
+    '''
+    MD5加密
+    '''
+    hl = hashlib.md5()
+    hl.update(string.encode('utf-8'))
+    return hl.hexdigest()
+
+
+def get_encoded_str(data: str, fill_str: str = '*', pre_len: int = 1, suf_len: int = 1) -> str:
+    '''
+    隱藏字串中指定部分
+
+    Args:
+        data (str): 原字串
+        fill_str (str): 取代隱藏部分的字元
+        pre_len (int): 前面保留的字元數量
+        suf_len (int): 後面保留的字元數量
+
+    Returns:
+        str: 結果字串
+    '''
+    s = ""
+    if len(data) <= 2:
+        s = data[:pre_len].ljust(2, fill_str)
+    else:
+        s = data[:pre_len] + (fill_str*(len(data)-(pre_len+suf_len))) + data[-suf_len:]
+    return s
 
 
 if __name__ == '__main__':
